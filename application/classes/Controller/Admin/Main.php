194d47182a46ac3b5389b4958c3657cab2f6765c
mrozk <mrozk2012@gmail.com>
2014-10-31 12:18:28 +0200

    zzzzzzzzzzzz

--- application/classes/Controller/Admin/Main.php
+++ application/classes/Controller/Admin/Main.php
@@ -238,8 +238,8 @@ class Controller_Admin_Main extends Controller_Admin_Layout{
     // Главная страница
     public function action_index(){
         $session = Session::instance();
-        $insalesuser = $session->set('xxxx','rrrr');
-        echo $insalesuser->get('xxxx');
+        //$insalesuser = $session->set('xxxx','rrrr');
+        echo $session->get('xxxx');
         $query = DB::select()->from('insalesusers')->as_object()->execute();
         $this->template->set('content', View::factory('admin/dashboard')->set('insalesusers',$query));
     }
