b53af68fda5532b91dd4ea2c6e954e637845ee97
mrozk <mrozk2012@gmail.com>
2014-10-31 19:10:23 +0200

    zzzzzzzzzzzz

--- application/classes/Controller/Cabinet.php
+++ application/classes/Controller/Cabinet.php
@@ -477,6 +477,7 @@ class Controller_Cabinet extends  Controller_Base{
         $insales_id = $session->get('token_insales_id');
 
         $insales_user = ORM::factory('InsalesUser', array('insales_id' => $insales_id));
+        print_r($insales_user);
         if( $insales_user->loaded() ){
             if( $insales_token == md5( $token . $insales_user->passwd ) ){
 
