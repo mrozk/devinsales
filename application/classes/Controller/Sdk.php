b58bfd29c866ccd982a60397524690a92bae2b25
mrozk <mrozk2012@gmail.com>
2014-11-02 22:32:11 +0200

    xxxxxxxxxx

--- application/classes/Controller/Sdk.php
+++ application/classes/Controller/Sdk.php
@@ -331,7 +331,8 @@ class Controller_Sdk extends Controller
         return $result_products;
     }
     public function action_index(){
-        $token = $this->request->query('tosken');
+        //$token = $this->request->query('tosken');
+        print_r( $_GET );
         print_r($this->request);
         echo 'xxx';
         echo $token;
