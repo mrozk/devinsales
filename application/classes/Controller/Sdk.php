5563021277a20b11c76f59e21e9748905d34feab
mrozk <mrozk2012@gmail.com>
2014-11-02 23:30:34 +0200

    xxxxxxxxxx

--- application/classes/Controller/Sdk.php
+++ application/classes/Controller/Sdk.php
@@ -350,7 +350,6 @@ class Controller_Sdk extends Controller
 
              if( isset($items) && !empty( $items ) ){
                  $info['cart'] = $this->getItemsFromInsales($url['scheme'] . '://' . $url['host'], $items, $settingsToIntegrator);
-                 print_r( $info );
                  MemController::getMemcacheInstance()->set( $card, json_encode( $info ), 0, 1200  );
              }
              try{
