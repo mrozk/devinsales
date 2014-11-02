9f827886eb154a2f0c53bd4750153cebfde9b7ec
mrozk <mrozk2012@gmail.com>
2014-11-02 16:56:02 +0200

    xxxxxxxxxx

--- application/classes/Controller/Sdk.php
+++ application/classes/Controller/Sdk.php
@@ -333,7 +333,8 @@ class Controller_Sdk extends Controller
     public function action_index(){
          $token = $this->request->query('token');
          $items = $this->request->query('items');
-         echo    $token;
+        echo 'xxx';
+         echo  $token;
          $has_token = MemController::getMemcacheInstance()->get( 'card_' . $token );
 
          if($has_token){
