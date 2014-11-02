865e3f0bf504a1e757e99eae86f12466fb20f842
mrozk <mrozk2012@gmail.com>
2014-11-02 23:07:37 +0200

    xxxxxxxxxx

--- application/classes/Controller/Sdk.php
+++ application/classes/Controller/Sdk.php
@@ -342,7 +342,7 @@ class Controller_Sdk extends Controller
         //echo $items;
          //$has_token = MemController::getMemcacheInstance()->get( 'card_' . $token );
         $instance = MemController::getMemcacheInstance();
-
+        echo $instance->get($card);
          if($has_token){
              $info = json_decode( $has_token, true );
              $settings = MemController::initSettingsMemcache($info['host']);
