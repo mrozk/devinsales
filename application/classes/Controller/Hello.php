86dee9c90af48b5b1c8d9a86af54b3216992d57f
mrozk <mrozk2012@gmail.com>
2014-11-05 22:17:02 +0200

    xxxxxxxxxx

--- application/classes/Controller/Hello.php
+++ application/classes/Controller/Hello.php
@@ -11,7 +11,7 @@ class Controller_Hello extends Controller
     }
     public function action_gus()
     {
-        header('Content-Type: text/javascript; charset=UTF-8');
+        //header('Content-Type: text/javascript; charset=UTF-8');
         $id = (int)$this->request->param('id');
         $settings = MemController::initSettingsMemcache( $id );
 
